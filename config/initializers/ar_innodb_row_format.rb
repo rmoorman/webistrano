234943d61f2e13f9f5d136cc0139dd774ebf85a3
けいそ <sasrai@gmail.com>
2015-05-08 21:39:57 +0900

    sqlite3混在環境でMySQLのutf8mb4対応パッチがNameErrorを吐く問題を修正
    「ActiveRecordをutf8mb4で動かす」をsqlite3併用環境でも動かす
    http://qiita.com/nekoruri/items/a91cc2654e2d0535f7b4

--- config/initializers/ar_innodb_row_format.rb
+++ config/initializers/ar_innodb_row_format.rb
@@ -1,15 +1,19 @@
 ActiveSupport.on_load :active_record do
-  module ActiveRecord::ConnectionAdapters
+  begin
+    module ActiveRecord::ConnectionAdapters
 
-    class AbstractMysqlAdapter
-      def create_table_with_innodb_row_format(table_name, options = {})
-        table_options = options.merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
-        create_table_without_innodb_row_format(table_name, table_options) do |td|
-          yield td if block_given?
+      class AbstractMysqlAdapter < AbstractAdapter
+        def create_table_with_innodb_row_format(table_name, options = {})
+          table_options = options.reverse_merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+          create_table_without_innodb_row_format(table_name, table_options) do |td|
+            yield td if block_given?
+          end
         end
+        alias_method_chain :create_table, :innodb_row_format
       end
-      alias_method_chain :create_table, :innodb_row_format
-    end
 
+    end
+  rescue NameError => ex
+    # MySQLが使われていないので無視
   end
 end
